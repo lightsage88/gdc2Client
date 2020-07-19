4479e0ca3c5af0fc1df1e83db6f94066a157d38e
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-07-19 01:08:04 -0700

    1

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome', 'ChromeHeadless', ' ChromeHeadlessNoSandbox'],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
