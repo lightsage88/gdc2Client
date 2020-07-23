89a813cb7a1e183561342d4313173b3e97d61dc4
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-07-22 19:15:18 -0700

    addign the s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE_URL: "http://fast-peak-68791.herokuapp.com/",
+  API_BASE_URL: "https://fast-peak-68791.herokuapp.com/",
   JWT_SECRET: "FELIBO",
   JWT_EXPIRY: "1d"
 };
